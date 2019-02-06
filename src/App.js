1ee2dd3ae8188bd761b256f1683335f83dd23f5e
Ali Torki <ali_4286@live.com>
2019-02-06 21:15:54 +0330

    chore: todolist wrapper tag and relocation todoList file path to containers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
+import styled from "@emotion/styled";
 import { Layout } from "antd";
 
-import TodoList from "./components/todoList";
-
-import { observer, inject } from "mobx-react";
+import TodoList from "./containers/TodoList";
 
 import "./scss/app.scss";
 
 const { Content, Footer } = Layout;
+const Wrapper = styled.div`
+	width: 560px;
+	/* min-height: 300px; */
+	margin: 50px auto;
+	background-color: #fff;
+	border-radius: 25px;
+	padding: 20px;
+	box-shadow: rgba(50, 50, 50, 0.1) 0px 10px 35px;
+`;
 
-@inject("todoList")
-@observer
 class BootCamp extends Component {
 	render() {
 		return (
 			<Layout style={{ minHeight: "100vh" }}>
 				<Layout style={{ marginRight: 30 }}>
 					<Content style={{ margin: "0 16px" }}>
-						<div className="app-content">
+						<Wrapper>
 							<TodoList />
-						</div>
+						</Wrapper>
 					</Content>
 					<Footer style={{ textAlign: "center" }}>Made with Love by Ali Torki</Footer>
 				</Layout>
